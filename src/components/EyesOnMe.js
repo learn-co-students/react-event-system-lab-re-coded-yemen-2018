412358f1a80530c86560c9d6c7343b0a1a406679
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-12 15:58:39 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
-// Code EyesOnMe Component Here
+// Code Keypad Component Here
+
+import React from 'react';
+
+class EyesOnMe  extends React.Component {
+ 
+  focusEvent = () => {
+    console.log('Good!');
+  }
+  
+    blurEvent = () => {
+    console.log( 'Hey! Eyes on me!');
+  }
+ 
+  render() {
+    return (
+      <button type="password" onFocus={this.focusEvent} onBlur={this.blurEvent}> click</button>
+    );https://learn.co/tracks/re-coded-front-end-web-development/react/state-and-events/event-system-lab#
+  }
+}
+
+export default EyesOnMe ;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+
+import React from 'react';
+
+class Keypad extends React.Component {
+ 
+  Keypad = () => {
+    console.log('Entering password...');
+  }
+ 
+  render() {
+    return (
+      <input type="password" onKeyUp ={this.Keypad} />
+    );
+  }
+}
+
+export default Keypad;
