c7552a02145a49da51947cc483128a28acbde63a
Salah Al-Dhaferi <binaryminded@gmail.com>
2018-10-19 07:59:13 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class EyesOnMe extends React.Component {
+  focus = () =>{
+    console.log('Good!');
+  }
+  blur = () =>{
+    console.log('Hey! Eyes on me!');
+  }
+  render(){
+    return(
+      <button onFocus={this.focus} onBlur={this.blur}>Click me!</button>
+    )
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Keypad extends React.Component {
+  isTyping = () =>{
+    console.log('Entering password...');
+  }
+  render(){
+    return(
+      <input type="password" onKeyUp ={this.isTyping}/>
+    )
+  }
+}
